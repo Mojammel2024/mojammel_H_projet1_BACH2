be06e3939e4e67c9eb8d132c7427715fd80deb13
Mojammel2024 <ahadmozammel7@gmail.com>
2024-12-06 14:03:08 +0100

    last done

--- new_app/src/src_components/TaskDetailsPage.jsx
+++ new_app/src/src_components/TaskDetailsPage.jsx
@@ -20,7 +20,7 @@ import {
   FooterSections,
   FooterSection,
   FooterBottom
-} from './StylesCompo/TaskDetailsPage.styles'; // Import des styles
+} from './TaskDetailsPage.styles'; // Import des styles
 
 const TaskDetailsPage = () => {
   const navigate = useNavigate();
